56afa477243d1e0f299df6bedf3cb51ae07c172a
manojmarquez33 <manoj17kumar10@gmail.com>
2024-03-18 13:57:11 +0530

    Pre-release 1.1

---
+++ Login screen 3/app/src/main/java/com/manomar/loginscreen/MainActivity.java
@@ -0,0 +1,48 @@
+package com.manomar.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button login;
+    EditText email,password;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.et_email);
+        password = findViewById(R.id.et_password);
+        login = findViewById(R.id.btnlogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mail = email.getText().toString();
+                String pass = password.getText().toString();
+
+                if(mail.equals("manoj17kumar10@gmail.com") && pass.equals("1426"))
+                {
+                    Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                    startActivity(intent);
+                }else
+                {
+                    Toast.makeText(MainActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
+    }
+
+}
